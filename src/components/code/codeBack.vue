0124a842a384e6d53903ddc9780156ac1058ed09
yuanshuai <yuanshuai5230@163.com>
2017-12-19 22:41:46 +0800

    20171219 22:30

--- src/components/code/codeBack.vue
+++ src/components/code/codeBack.vue
@@ -5,18 +5,19 @@
 	    		<el-option v-for="item in options" :key="item.label" :label="item.label" :value="item.value" >
 	    		</el-option>
 			</el-select><br><br>
-			<el-tag type="success" v-show="path">代码路径：{{path}}</el-tag>
+			<el-tag type="success" v-show="path">代码路径：{{path}}</el-tag><br>
+			<el-tag type="success" v-show="project">所属项目：{{project}}</el-tag>
 		</div>
 		<div id="cbtable">
-			<el-table :data="projectdir">
-				<el-table-column label="备份目录">
+			<el-table :data="projectdir" :default-sort = "{prop: 'date', order: 'descending'}">
+				<el-table-column label="备份目录" prop="date" sortable>
 					<template slot-scope="scope">
 						{{scope.row.dir}}
 					</template>
 				</el-table-column>
 				<el-table-column label="操作">
 					<template slot-scope="scope">
-						<el-button size="mini" type="primary">回退</el-button>
+						<el-button size="mini" type="primary" @click="rollbackdir(scope.row.dir)">回退</el-button>
 					</template>
 				</el-table-column>
 			</el-table>
@@ -33,6 +34,7 @@
 				value: '',
 				projectdir: [],
 				path: '',
+				project: '',
 			}
 		},
 		methods: {
@@ -40,17 +42,31 @@
 				console.log(value)
 				if (value) {
 					axios.get('/api/backstage/prodir',{params:{'dirname':value}}).then(resp => {
-					console.log(resp.data.prodir);
-					console.log(resp.data.path);
+					// console.log(resp.data.prodir);
+					// console.log(resp.data.path);
 					this.path = resp.data.path
 
 					this.projectdir = resp.data.prodir
+					this.project = resp.data.project
 					}).catch(err => {console.log(err)})
 				}
 			},
 			clear() {
 				this.path = '';
 				this.projectdir = [];
+				this.project = '';
+			},
+			rollbackdir(dir) {
+				axios({
+					method: 'post',
+					url: '/api/backstage/rollbackpath',
+					data: [this.project,dir],
+					transformRequest:[
+						function(data) {
+							return 'project='+data[0]+'&sdir='+data[1]
+						}
+					]
+				}).then(resp => {console.log(resp)}).catch(err => {console.log(err)})
 			}
 		},
 		mounted(){
