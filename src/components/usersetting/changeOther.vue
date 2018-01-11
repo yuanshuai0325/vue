e24ea75bdab4d37a34ae04430f8cdea28bff0c29
yuanshuai <yuanshuai@conac.com>
2018-01-11 16:35:38 +0800

    20180111 16:40

---
+++ src/components/usersetting/changeOther.vue
@@ -0,0 +1,13 @@
+<template>
+	<div id="changeother">
+		<span>yyyyyyyyyy changeother yyyyyyyy</span>
+	</div>
+</template>
+
+<script>
+	
+</script>
+
+<style scoped>
+	
+</style>
---
+++ src/components/usersetting/changePw.vue
@@ -0,0 +1,13 @@
+<template>
+	<div id="changepw">
+		<span>xxxxxx changepw xxxxxx</span>
+	</div>
+</template>
+
+<script>
+	
+</script>
+
+<style scoped>
+	
+</style>
